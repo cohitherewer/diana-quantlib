72af5228e73a37383ed84a6bec6c79a90c00a907
Tim Verbelen <tim.verbelen@ugent.be>
2023-01-13 11:19:34 +0100

    add setup.py to be able to install as python package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="diana",
+    version="0.1",
+    description="A package to train and integerise QNNs with the DIANA chip.",
+    author="Abubakr Nada",
+    author_email="abubakr_ehab@hotmail.com",
+    packages=find_packages(),
+    install_requires=["torch", "networkx"],
+)
