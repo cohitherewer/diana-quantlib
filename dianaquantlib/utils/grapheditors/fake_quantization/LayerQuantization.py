69e4d316b2cada8611111ea3e7dc9eb2056a8cde
Maarten Vandersteegen <maartenvandersteegen@gmail.com>
2023-06-30 10:56:07 +0200

    Small improvement in determining the aligned input quantization scales of add op

--- dianaquantlib/utils/grapheditors/fake_quantization/LayerQuantization.py
+++ dianaquantlib/utils/grapheditors/fake_quantization/LayerQuantization.py
@@ -16,8 +16,23 @@ class AddTreeAlignInputScalesApplier(Applier):
         """Ensure both input scales of add are equal
         """
         in_modules = [g.get_submodule(n.target) for n in ap.inbound_frontier]
-        scales = [mod.scale for mod in in_modules]
-        aligned_scale = torch.concat(scales).max().unsqueeze(0)
+        user_nodes = list(ap.root.users)
+        assert len(user_nodes) == 1, "TODO: currently don't know what to do with zero or multiple user nodes of add op"
+        out_module = g.get_submodule(user_nodes[0].target)
+        in_scales = [mod.scale for mod in in_modules]
+        out_scale = out_module.scale
+
+        # Rules for determining the scale:
+        # 1) aligned value of input scales <= output scale
+        #    motivation: The sum of two values is usually a larger number so it does not make sense
+        #                to make both input scales > output scale.
+        #                Violating this rule can also introduce an output division op with division factor < 1
+        #                after integerization, resulting in an add op that cannot be performed
+        #                on the digital accelerator (div factor must be >= 1)
+        # 2) prefer to use the largest input scale as aligned scale value (not violating the previous rule)
+        #    motivation: Prefer range over precision
+        aligned_scale = torch.min(torch.concat(in_scales).max().unsqueeze(0), out_scale)
+
         for mod in in_modules:
             mod.scale = aligned_scale
 
