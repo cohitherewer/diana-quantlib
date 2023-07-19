11cc055103fce95f2fa445cc0bfe8dd58e4c2eb9
Maarten Vandersteegen <maartenvandersteegen@gmail.com>
2023-07-19 10:03:08 +0200

    Added mobilenetv2 to utils.py
    * Model works
    * Also removed imagenet model folder

--- examples/utils.py
+++ examples/utils.py
@@ -7,12 +7,14 @@ from dianaquantlib.utils.BaseModules import DianaModule
 from dianaquantlib.models.mlperf_tiny import ResNet, MobileNet, DAE, DSCNN
 from dianaquantlib.models.cifar10.cifarnet import CifarNet8
 from dianaquantlib.models.cifar10.resnet import resnet20
+from dianaquantlib.models.cifar10.mobilenetv2 import MobileNetV2
 
 image_classifier_models = {
     'resnet8': ResNet,
     'resnet20': partial(resnet20, num_classes=12),
     'cifarnet8': CifarNet8,
     'mobilenetv1': partial(MobileNet, num_classes=12),
+    'mobilenetv2': partial(MobileNetV2, num_classes=12, width_mult=0.25),
 }
 
 audio_classifier_models = {
